0f001f656ccd20236900bf71dec095dba4869766
Kai Kuller <kavakai@gmail.com>
2022-03-08 09:34:40 -0700

    Add key to orders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 
   componentDidMount() {
     getOrders()
+      .then(data => this.setState(data))
       .catch(err => console.error('Error fetching:', err));
   }
 
